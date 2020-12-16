01986d68a52e64941ad34e2a0db8864fc77f94d3
Manikandan9821 <69073040+Manikandan9821@users.noreply.github.com>
2020-12-16 15:18:55 +0530

    Update app_url.dart

--- lib/logics/constants/app_url.dart
+++ lib/logics/constants/app_url.dart
@@ -1,5 +1,5 @@
 class AppUrl {
   static const BASE_URL = 'http://api.backendless.com/';
-  static const API_KEY = 'D8065324-3648-BA75-FF13-B8D839BA0400/';
-  static const APP_ID = '72858A17-7CA1-452C-9390-EC00F1A5F9A8/';
+  static const API_KEY = 'YOUR API_KEY/';
+  static const APP_ID = 'YOUR APP_ID/';
 }
